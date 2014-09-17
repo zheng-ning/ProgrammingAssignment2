5cab626dbf6f9317999c38f0cee79ba888e66ef5
zheng-ning <nyeon@yahoo.com>
2014-09-17 22:04:48 +0800

    upload assignment by Johnny ZN

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## makeCacheMatrix creates a special "matrix" object that can cache its inverse.
+## acheSolve computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed),
+## then the cachesolve should retrieve the inverse from the cache.
+makeCacheMatrix <- function(x = matrix()) { 
+        m <- NULL # declare a cariable named m and assign it to NULL
+        set <- function(y) { # create a function named set takes argument of y
+                x <<- y # assign y to x
+                m <<- NULL # what is m and where is m come from?
+        }
+        get <- function() x # x is the return value
+        setsolve <- function(solve) m <<- mean # takes mean as arg return m
+        getsolve <- function() m # return m 
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { 
+# In this specific case it gives us ability to pass additional arguments to mean function
+        m <- x$getsolve() # getmean x
+        if(!is.null(m)) { # check if it is empty
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get() # get data 
+        m <- solve(data, ...) # calculate mean
+        x$setsolve(m) # set mean
+        m
 }
